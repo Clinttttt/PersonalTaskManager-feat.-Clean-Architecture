7d9883f2a65b6913cf5da68caebe4a847a000dec
Clinttttt <clintvillanueva82@gmail.com>
2025-07-30 23:42:52 +0800

    updated responsive design + modern design

--- PTMPersonalTaskManager.Client/Services/PageState.cs
+++ PTMPersonalTaskManager.Client/Services/PageState.cs
@@ -1,8 +1,12 @@
-﻿namespace PTMPersonalTaskManager.Client.Services
+﻿using PTMPersonalTaskManager.Client.Components.Pages.Menupage.Note;
+
+namespace PTMPersonalTaskManager.Client.Services
 {
     public class PageState
     {
-     public bool ShowProfile { get; private set; }
+        public bool ShowProfile { get; private set; }
+        public bool HideCard { get; private set; }
+        public bool DisplayNotes { get; private set; }
         public event Action? OnChange;
 
         public void Show()
@@ -14,8 +18,17 @@
         public void Reset()
         {
             ShowProfile = false;
+            HideCard = true;
+            DisplayNotes = false;
             NotifyChanges();
         }
         private void NotifyChanges() => OnChange?.Invoke();
+
+        public void Hide()
+        {
+            HideCard = false;
+            DisplayNotes = true;
+            NotifyChanges();
+        }
     }
 }
